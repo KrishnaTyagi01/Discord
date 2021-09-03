6c7f1485ccf025d123a9cdd5ec499baeac9d37db
KrishnaTyagi01 <tyagikrishna38@gmail.com>
2021-09-03 15:43:04 +0530

    minor changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  //config file here
+  apiKey: "AIzaSyDSR7z9JoxFXzwy15c6gygPjRQvpzwIs0Q",
+  authDomain: "discord-clone-f9444.firebaseapp.com",
+  projectId: "discord-clone-f9444",
+  storageBucket: "discord-clone-f9444.appspot.com",
+  messagingSenderId: "887418576400",
+  appId: "1:887418576400:web:95946b14ea1d4c60833495",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
