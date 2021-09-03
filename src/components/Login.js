4e32d3e990262d2f2d8af53a83c3058b77330d32
KrishnaTyagi01 <tyagikrishna38@gmail.com>
2021-09-03 15:57:16 +0530

    minor changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import '../css/Login.css';
-import { Button } from '@material-ui/core';
-import {auth, provider} from '../firebase';
-
+import React from "react";
+import "../css/Login.css";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "../firebase";
+import logo from "../css/discord.png";
 function Login() {
-    const signin = ()=>{
-        auth.signInWithPopup(provider)
-        .catch(error=>alert(error.message));
-    }
+  const signin = () => {
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
 
+  return (
+    <div className="login">
+      <div className="login__logo">
+        <img src={logo} alt="logo" />
+      </div>
 
-    return (
-        <div className="login">
-            <div className="login__logo">
-                <img src="https://upload.wikimedia.org/wikipedia/sco/thumb/9/98/Discord_logo.svg/1200px-Discord_logo.svg.png"
-                 alt="logo"/>
-            </div>
-
-            <Button onClick={signin}>Sign In</Button>
-        </div>
-    )
+      <Button onClick={signin}>Sign In</Button>
+    </div>
+  );
 }
- 
-export default Login
+
+export default Login;
